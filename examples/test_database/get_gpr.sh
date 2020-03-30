c1f3a93d4f6e814c11b8f53bc5301ca2864b98f4
msdsufz <msdsufz@ufz.de>
2020-03-30 10:17:55 +0200

    Add existing file

--- examples/test_database/get_gpr.sh
+++ examples/test_database/get_gpr.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-find $1 -name '*.fa' > $2 #Find and save all KO ids from directory
+find $1 -name '*.fa' > temp.txt #Find and save all KO ids from directory
 
-sed -E 's/(.fa)+$//;s/.\///' $2 > $2 # remove ./ from the beginning of each line and .fa and store in a new file
+sed -E 's/(.fa)+$//;s/.\///' temp.txt > $2 # remove ./ from the beginning of each line and .fa and store in a new file
 
 java -jar $3 $2 $4
