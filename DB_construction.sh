a33f7331b2f2e529c65409620e1a97c3c2ff3461
mdsufz <53437593+mdsufz@users.noreply.github.com>
2021-04-14 16:24:26 +0200

    Add files via upload

---
+++ DB_construction.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+ 
+# $1 - project full path (It needs to be inside the OrtSuite main folder. Ex: "~/OrtSuite/PROJECT_NAME/")
+# $2 - the flag of the list (-m, -r, -e, or -k) 
+# $3 - full path of the identifiers list
+# $4 - full path of Ortsuite instalation
+ 
+mkdir "$1"/database
+ 
+printf "%s\n" " " "seq_download_started" " "  
+ 
+download_kos -o "$1"/database "$2" "$3"
+ 
+printf "%s\n" " " "seq_download_finished" " "
+ 
+# Copy "get_gpr.sh" from the OrtSuite folder to the database folder.
+#cp "$1"/../get_gpr.sh "$1"/database
+
+# if it is not in the main folder, run the command below:
+cp "$4"/examples/test_database/get_gpr.sh "$1"/database
+
+printf "%s\n" "Starting GPR collection" " "
+
+sh "$1"/database/get_gpr.sh "$1"/database/KO_grp.txt "$4"/../GPRKO.jar "$1"/database/final_gpr.xlsx
+ 
+printf "%s\n" " " "Finished GPR collection - Do manual check of file" " "
+ 
+# Done
+
---
+++ annotate_and_predict.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+ 
+# $1 - project folder's full path (ex: ~/OrtSuite/examples/ )
+# $2 - orthof results folder's full path
+# $3 - user_input.csv file's full path
+ 
+mkdir "$1"/work_dir
+printf "%s\n"
+ 
+work_dir="$1"/work_dir # location where you want to store the results
+database="$1"/database # location of the ORAdb (where the FASTA files are located)
+orthof="$2" # location of the folders with the results from OrthoFinder
+user_input="$3"
+ 
+create_project -out $work_dir -db $database
+printf "%s\n"
+ 
+relaxed_search -wd $work_dir -of $orthof -t 2
+printf "%s\n"
+ 
+restrictive_search -wd $work_dir -t 2
+printf "%s\n"
+ 
+annotation -wd $work_dir 
+printf "%s\n"
+ 
+mkdir $work_dir/interactions
+printf "%s\n"
+ 
+Rscript "$1"/../gpr_manipulation.R -n $database/final_gpr.xlsx -s $work_dir/Results/Species_Annotation.csv -u $user_input -o $work_dir/interactions
+printf "%s\n" "Done" " "
+ 
+#Done
